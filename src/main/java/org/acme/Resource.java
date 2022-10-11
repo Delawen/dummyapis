15581262769fa739ff4997636b0bff3746250a72
Maria Arias de Reyna <ariasdereyna@redhat.com>
2022-10-11 13:26:57 +0200

    Adding dummy apis for criminal records and lab results

---
+++ src/main/java/org/acme/Resource.java
@@ -0,0 +1,68 @@
+package org.acme;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Random;
+
+@Path("/")
+public class Resource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/suspect")
+    public Integer suspect() {
+        return 93634870 + new Random().nextInt(10);
+    }
+
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/suspect")
+    public Integer pso() {
+        return suspect();
+    }
+
+    @GET
+    @Path("/accomplices")
+    @Produces("image/jpg")
+    public byte[] photo() throws IOException {
+        URL url = new URL("https://thispersondoesnotexist.com/image");
+        final URLConnection connection = url.openConnection();
+        try (InputStream input = connection.getInputStream();
+             ByteArrayOutputStream output = new ByteArrayOutputStream()) {
+            final byte[] buffer = new byte[8192];
+            for (int count; (count = input.read(buffer)) > 0;) {
+                output.write(buffer, 0, count);
+            }
+            return output.toByteArray();
+        }
+    }
+
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/labResults/element")
+    public Integer labResults() {
+        return 37894 + new Random().nextInt(10);
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/labResults/researcher")
+    public String labResultsDescription() {
+        String[] names = new String[]{"María Carmen", "María", "Carmen", "Josefa", "Ana María", "Isabel",
+                "Pilar", "Dolores", "Teresa", "Laura", "Cristina", "Antonia"};
+        String[] lastnames = new String[]{"Wang", "Li", "García", "Smith", "Yang", "Fernández", "Díaz", "Arias"};
+        final var name = names[new Random().nextInt(names.length - 1)];
+        final var lastname = lastnames[new Random().nextInt(lastnames.length - 1)];
+        return name + " " + lastname;
+    }
+
+}
