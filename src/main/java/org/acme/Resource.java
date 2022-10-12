814c79bef6878f5f95a2ca8ec767e18f47aa7674
Maria Arias de Reyna <ariasdereyna@redhat.com>
2022-10-12 08:00:31 +0200

    Adding POST versions of the services.

--- src/main/java/org/acme/Resource.java
+++ src/main/java/org/acme/Resource.java
@@ -17,20 +17,20 @@ public class Resource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    @Path("/suspect")
+    @Path("/criminal-identity/suspect")
     public Integer suspect() {
         return 93634870 + new Random().nextInt(10);
     }
 
     @POST
     @Produces(MediaType.TEXT_PLAIN)
-    @Path("/suspect")
+    @Path("/criminal-identity/suspect")
     public Integer pso() {
         return suspect();
     }
 
     @GET
-    @Path("/accomplices")
+    @Path("/criminal-identity/accomplices")
     @Produces("image/jpg")
     public byte[] photo() throws IOException {
         URL url = new URL("https://thispersondoesnotexist.com/image");
@@ -52,11 +52,29 @@ public class Resource {
     public Integer labResults() {
         return 37894 + new Random().nextInt(10);
     }
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/labResults/element")
+    public Integer plabResults() {
+        return 37894 + new Random().nextInt(10);
+    }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/labResults/researcher")
     public String labResultsDescription() {
+        String[] names = new String[]{"María Carmen", "María", "Carmen", "Josefa", "Ana María", "Isabel",
+                "Pilar", "Dolores", "Teresa", "Laura", "Cristina", "Antonia"};
+        String[] lastnames = new String[]{"Wang", "Li", "García", "Smith", "Yang", "Fernández", "Díaz", "Arias"};
+        final var name = names[new Random().nextInt(names.length - 1)];
+        final var lastname = lastnames[new Random().nextInt(lastnames.length - 1)];
+        return name + " " + lastname;
+    }
+
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/labResults/researcher")
+    public String plabResultsDescription() {
         String[] names = new String[]{"María Carmen", "María", "Carmen", "Josefa", "Ana María", "Isabel",
                 "Pilar", "Dolores", "Teresa", "Laura", "Cristina", "Antonia"};
         String[] lastnames = new String[]{"Wang", "Li", "García", "Smith", "Yang", "Fernández", "Díaz", "Arias"};
